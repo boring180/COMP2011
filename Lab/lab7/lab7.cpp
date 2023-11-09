b2f9ab78669c0453b208720548eccebddcb7263c
Boron <borongxu@outlook.com>
2023-11-09 18:26:39 +0800

    Lab7 done

--- Lab/lab7/lab7.cpp
+++ Lab/lab7/lab7.cpp
@@ -11,5 +11,20 @@ using namespace std;
 // Students at a higher year of study should be ordered in front.
 // You can use bubble sort for the reordering. The detail instruction is on Lab7 webpage.
 void reorder_student(Student* queue [], int num_students) {
-
+    for(int sort_iteration = 0; sort_iteration < num_students - 1; sort_iteration++)
+    {   
+        // cout<<"sort_iteration"<<sort_iteration<<endl;
+        for(int index = 0;index < num_students - sort_iteration - 1;index++)
+        {   
+            if(queue[index] == nullptr)break;
+            // cout<<"index"<<index<<endl;
+            if(queue[index]->yearOfStudy < queue[index+1]->yearOfStudy)
+            {
+                Student temp = *queue[index];
+                *queue[index] = *queue[index+1];
+                *queue[index+1] = temp;
+            }
+        }
+    }
+    return;
 }
